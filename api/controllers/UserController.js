729b725ac0b65748661e679346616db1f579d070
Maarten van Willigen <mmaartenvanwilligen@gmail.com>
2018-01-14 18:04:48 +0100

    dummydata action

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -79,8 +79,34 @@ module.exports = {
         // Otherwise if this is an HTML-wanting browser, do a redirect.
         return res.redirect('user/login');
 
+    },
+
+    dummydata: function (req, res, next) {
+
+        var dummyData = [
+            {"name": "John", "email": "john@mail.nl", "password": "password", "role": "user"},
+            {"name": "User", "email": "user@mail.nl", "password": "password", "role": "user"},
+            {"name": "Admin", "email": "admin@mail.nl", "password": "password", "role": "admin"},
+            {"name": "Maarten", "email": "maarten@mail.nl", "password": "password", "role": "user"},
+            {"name": "Felipe", "email": "felipe@mail.nl", "password": "password", "role": "user"},
+            {"name": "iris", "email": "iris@mail.nl", "password": "password", "role": "user"}
+        ];
+
+
+        User.create(dummyData).exec(function (err, user) {
+            if (err && err.invalidAttributes) {
+
+                console.log(err.Errors);
+
+                return res.ok();
+
+            } else {
+                console.log('name is:', user.name);
+                return res.ok();
+            }
+        });
+
     }
 
 
-}
-;
+};
